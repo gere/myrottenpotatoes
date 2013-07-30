ab6d5d9e3da764c4664b1f42bae2054b10fe0ec2
simone <gerevini@gmail.com>
2013-07-30 01:44:09 -0700

    added all crud controllers and view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,20 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    id = params[:id]
+    @movie = Movie.find(id)
+  end
+
+  def new
+    #default: render 'new template'
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
 end
 
